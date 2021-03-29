283190e72188cdf5433f624c551802b3e197c615
Johannes Herold <johannes.herold2@tu-dresden.de>
2021-03-29 17:50:49 +0200

    Some changes to price calculations

--- EnchantmentCalculator.py
+++ EnchantmentCalculator.py
@@ -2,39 +2,73 @@
 
 import math
 import sys
-from anytree import Node, NodeMixin, RenderTree, DoubleStyle
+from anytree import Node, RenderTree, DoubleStyle
 import itertools
 from random import randint
 
 
+name_lut = ["None", "Protection", "Fire Protection", "Feather Falling", "Blast Protection", "Projectile Protection", "Thorns", "Respiration", "Depth Strider", "Aqua Affinity", "Sharpness", "Smite", "Bane of Arthropods", "Knockback", "Fire Aspect", "Looting", "Efficiency", "Silk Touch", "Unbreaking", "Fortune", "Power", "Punch", "Flame", "Infinity", "Luck of the Sea", "Lure", "Frost Walker", "Mending", "Curse of Binding", "Curse of Vanishing", "Impaling", "Riptide", "Loyalty", "Channeling", "Multishot", "Piercing", "Quick Charge", "Soul Speed", "Sweeping Edge"]
+item_multiplier_lut = [0, 1, 2, 2, 4, 2, 8, 4, 4, 4, 1, 2, 2, 2, 4, 4, 1, 8, 2, 4, 1, 4, 4, 8, 4, 4, 4, 4, 8, 8, 4, 4, 1, 8, 4, 1, 2, 8, 4]
+book_multiplier_lut = [0, 1, 1, 1, 2, 1, 4, 2, 2, 2, 1, 1, 1, 1, 2, 2, 1, 4, 1, 2, 1, 2, 2, 4, 2, 2, 2, 2, 4, 4, 2, 2, 1, 4, 2, 1, 1, 4, 2]
+prior_work_penalty = [0, 1, 3, 7, 15, 31, 100]
+
+
 class Enchantment:
-	identifier = -1			# Default = -1, Sweeping Edge = 37
-	name = "Default"
-	level = 0
-	item_multiplier = 0
-	book_multiplier = 0
+	def __init__(self, identifier, level, keep = False):
+		if identifier < -1 or identifier > 37: self.identifier = -1
+		else: self.identifier = identifier
+		self.level = level
+		self.keep = keep
+		self.name = name_lut[self.identifier + 1]
+		self.item_multiplier = item_multiplier_lut[self.identifier + 1]
+		self.book_multiplier = book_multiplier_lut[self.identifier + 1]
 	
 	
 class Item(Node):
-	def __init__(self, name, parent = None, children = None, item_type = "book", cost = 0, enchantments = []):
-		self.item_type = item_type			# can be book or item. todo: differentiate between all different types of items to check enchantment compatibility
-		self.cost = cost
+	def __init__(self, name, parent = None, children = None, item_type = "book", prior_work = 0, enchantments = []):
+		self.item_type = item_type		# can be book or item. todo: differentiate between all different types of items to check enchantment compatibility
+		self.prior_work = prior_work
 		self.enchantments = enchantments
+		self.cost = 0
+		self.value = 0
 		self.name = name
 		self.parent = parent
 		if children:
 			self.children = children
 	
 	def updateCost(self):
-		# leaves can't update their cost, it is fixed
-		if self.is_leaf: return
+		# leaves can't update their children, also their prior work and enchantments are fixed and value is calculated differently
+		if not self.is_leaf:
+			self.children[0].updateCost()
+			self.children[1].updateCost()
+						
+			self.enchantments, incomp_penalty = combineEnchantments()
+			self.cost = prior_work_penalty[self.children[0].prior_work] + prior_work_penalty[self.children[1].prior_work] + self.children[1].value + incomp_penalty # + repairing + renaming
+			self.prior_work = 1 + min([max([self.children[0].prior_work, self.children[1].prior_work])], 5)		#  prior work caps at 6, which maps to cost of 100 (i.e. too expensive)
 		
-		# recursively update costs of all child nodes and add to own cost
-		self.cost = 0
-		for n in self.children:
-			n.updateCost()
-			self.cost = self.cost + n.cost
-
+			self.value = self.children[0].value + self.children[1].value
+			
+		else:
+			self.value = valueFromEnchantments(self.enchantments)
+			
+		# todo: reasons to break (figure out, how to propagate this break)
+		#  - cost is too expensive
+		#  - target is book, sacrifice is item
+		#  - enchantment marked as keep would get lost
+	
+	# Returns the combined enchantments from both children as well as the penalty for incompatible enchantments
+	def combineEnchantments(self):
+		return [], 0
+		
+	# Returns the value determined by an item's enchantments
+	def valueFromEnchantments(self):
+		value = 0
+		lut = self.item_type == "book" ? book_multiplier_lut : item_multiplier_lut
+		for e in self.enchantments:
+			value = value + e.level * lut[e.identifier]
+		
+		
+		
 
 def total_items(level):
 	res = 1
@@ -42,6 +76,26 @@ def total_items(level):
 		res = res + pow(2, level)
 		level = level - 1
 	return res
+	
+
+def get_best_combination(tree):
+	best_combination = 0
+	
+	# go through all possible combinations of start items
+	# temp: add random prices to each leave
+	for l in tree.leaves:
+		l.cost = randint(1,9)
+		
+	# recalculate all costs of parents
+	tree.updateCost()
+	
+	# formatted output
+	print()
+	print("===================")
+	print()
+	print(RenderTree(base_tree, style = DoubleStyle).by_attr(lambda n: n.name + ", Cost: " + str(n.cost)))
+	
+	return best_combination
 
 	
 total_inputs = 1
@@ -59,21 +113,23 @@ total_combinations = tree_layouts * combinations_per_tree
 
 #print("Total items: %i \nFull rows: %i \nAdditional leave pairs: %i \nPossible Positions for additional pairs: %i \nDifferent tree layouts: %i \nCombinations per tree: %i \nTotal combinations: %i" % (total_inputs, full_rows, additional_leave_pairs, possible_positions, tree_layouts, combinations_per_tree, total_combinations))
 
-all_items = []
-base_tree = Item("", cost = 0)
+# build base tree
+base_tree = Item("")
 # add two children to every leave until number of leaves matches possible_positions
 while len(base_tree.leaves) < possible_positions:
 	for n in base_tree.leaves:
-		Item("Target", parent = n, cost = 0)
-		Item("Sacrifice", parent = n, cost = 0)
+		Item("Target", parent = n)
+		Item("Sacrifice", parent = n)
 
 base_tree.name = "Final item"
 
 # remember base tree in order to return here later after each iteration
 base_tree_leaves = base_tree.leaves
 
+best_tree_variant = 0
+
 # only operate on base tree if no aditional pairs need to be added
-if (additional_leave_pairs == 0): print(RenderTree(base_tree).by_attr())
+if (additional_leave_pairs == 0): get_best_combination(base_tree)
 
 else:
 	# build a list of all leaves indices
@@ -81,42 +137,22 @@ else:
 	for i in range(0, len(base_tree_leaves)): indices.append(i)
 	
 	# go through all possible positions for additional leave pairs
+	tree_variant = 0
 	for p in itertools.combinations(indices, additional_leave_pairs):
 		
 		# add an additional leave pair at each position
 		for index in p:
-			Item("Target", parent=base_tree_leaves[index], cost = 0)
-			Item("Sacrifice", parent=base_tree_leaves[index], cost = 0)
+			Item("Target", parent=base_tree_leaves[index])
+			Item("Sacrifice", parent=base_tree_leaves[index])
 			
-		# tree is now ready
-		
-		# temp: add random prices to each leave
-		for l in base_tree.leaves:
-			l.cost = randint(1,9)
-			
-		# recalculate all costs of parents
-		base_tree.updateCost()
-			
-		print(RenderTree(base_tree, style = DoubleStyle).by_attr(lambda n: n.name + ", Cost: " + str(n.cost)))
-		print("===================")
-		print()
+		# tree is now ready		
+		get_best_combination(base_tree)
 		
 		# return to base tree for next iteration
 		for n in base_tree_leaves:
 			n.children = ()
-
-
-
-
-
-
-
-
-
-
-
-
-
+			
+		tree_variant = tree_variant + 1
 
 
 
